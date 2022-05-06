a63f0a818926c19dd14623eb24e9a4fa1a84702e
Tobias Simon <akkiirah@gmail.com>
2022-05-06 02:24:48 +0200

    Projektdateien hinzufügen.

---
+++ KoolSpaceShootah/Game.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+
+namespace MonoGameDesktopDX
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphicsManager;
+        SpriteBatch spriteBatch;
+
+        public Game1()
+        {
+            graphicsManager = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            
+            // TODO: load content here
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if(GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            {
+                this.Exit();
+            }
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime time)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            //TODO: Draw your game
+            base.Draw(time);
+        }
+    }
+}
